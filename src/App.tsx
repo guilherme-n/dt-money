9464c863bc0fdc367317060512cec77880ce4316
Guilherme Nascimento <guilherme.rdnascimento@gmail.com>
2023-03-04 20:39:12 -0300

    setup default css and theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,12 @@
+import { ThemeProvider } from 'styled-components';
+import { GlobalStyle } from './styles/global';
+import { defaultTheme } from './styles/themes/default';
+
 export function App() {
-	return <div className='App'></div>;
+	return (
+		<ThemeProvider theme={defaultTheme}>
+			<GlobalStyle />
+			<h1 className='App'>Hello world</h1>
+		</ThemeProvider>
+	);
 }
