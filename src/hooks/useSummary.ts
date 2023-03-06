b2a3fff47e83c939fcda24e0418277bfda75de4d
Guilherme Nascimento <guilherme.rdnascimento@gmail.com>
2023-03-06 01:30:59 -0300

    optimize summary calc

--- src/hooks/useSummary.ts
+++ src/hooks/useSummary.ts
@@ -1,23 +1,28 @@
+import { useMemo } from 'react';
 import { useTransactions } from '../contexts/TransactionsContext';
 
 export function useSummary() {
 	const { transactions } = useTransactions();
 
-	return transactions.reduce(
-		(acc, transaction) => {
-			if (transaction.type === 'income') {
-				acc.incomes += transaction.price;
-				acc.total += transaction.price;
-			} else {
-				acc.expenses += transaction.price;
-				acc.total -= transaction.price;
+	const summary = useMemo(() => {
+		return transactions.reduce(
+			(acc, transaction) => {
+				if (transaction.type === 'income') {
+					acc.incomes += transaction.price;
+					acc.total += transaction.price;
+				} else {
+					acc.expenses += transaction.price;
+					acc.total -= transaction.price;
+				}
+				return acc;
+			},
+			{
+				incomes: 0,
+				expenses: 0,
+				total: 0,
 			}
-			return acc;
-		},
-		{
-			incomes: 0,
-			expenses: 0,
-			total: 0,
-		}
-	);
+		);
+	}, [transactions]);
+
+	return summary;
 }
